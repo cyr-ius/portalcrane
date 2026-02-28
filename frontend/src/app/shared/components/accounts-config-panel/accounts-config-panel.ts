cd3ca05e223ed4442d28500bbd0b012949e5de61
cyr-ius <cyr-ius@ipocus.net>
2026-02-28 15:24:44 +0100

    Ad accounts panel

---
+++ frontend/src/app/shared/components/accounts-config-panel/accounts-config-panel.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: "app-accounts-config-panel",
+  imports: [],
+  templateUrl: "./accounts-config-panel.html",
+  styleUrl: "./accounts-config-panel.css",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AccountsConfigPanel {}
