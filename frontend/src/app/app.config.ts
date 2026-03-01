b204087035cbaa9618e707361106eaead02e62a2
cyr-ius <cyr-ius@ipocus.net>
2026-03-01 19:53:44 +0100

    REmove animation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,9 @@
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import {
   ApplicationConfig,
   provideZonelessChangeDetection,
 } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
-import { provideHttpClient, withInterceptors } from "@angular/common/http";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 import { routes } from "./app.routes";
 import { authInterceptor } from "./core/interceptors/auth.interceptor";
@@ -14,6 +13,5 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimationsAsync(),
   ],
 };
